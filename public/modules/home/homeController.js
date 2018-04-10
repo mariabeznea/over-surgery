77bad3e14d66a37028f090ddd6b4753b0ca884b2
Maria Beznea <mariabeznea@gmail.com>
2018-04-10 02:13:22 +0100

    added dashboard view

--- public/modules/home/homeController.js
+++ public/modules/home/homeController.js
@@ -1,12 +1,13 @@
 overSurgery.controller('homeController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     $scope.first_name = localStorage.first_name;
+    $scope.patient_id = localStorage.patient_id;
     $scope.token = localStorage.token;
 
     // HTTP GET ALL STAFF
     // $scope.staff = whateveryougetfromtherequest (needs to an array)
     // Do backend connection
-    $http.get('/api/patient').then(function (response) {
-        $scope.patients = response.data;
+    $http.get('/api/patient/' + $scope.patient_id).then(function (response) {
+        $scope.countInfo = response.data;
     });
 
     // //TODO: get the token only for authenticated users (it fails if the page reloads because the token didn't change)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,16 @@
 
         <!-- load bootstrap via CDN-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/sticky-footer-navbar.css" >
 
         <link rel="stylesheet" href="/css/colors.css">
         <link rel="stylesheet" href="/css/buttons.css">
         <link rel="stylesheet" href="/css/forms.css">
+        <link rel="stylesheet" href="/css/boxes.css" >
         <link rel="stylesheet" href="/css/utilities.css">
         <link rel="stylesheet" href="/css/sidebar.css">
         <link rel="stylesheet" href="/css/calendar.css">
+        <link rel="stylesheet" href="/css/sticky-footer-navbar.css" >
+
 
 
 
